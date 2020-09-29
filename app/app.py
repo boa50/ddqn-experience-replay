d676e24f7231746dbbee4ff7149222fabdb32ace
boa50 <bruno.albuquerque50@gmail.com>
2020-09-29 05:49:11 -0300

    Função para plotagem das estatísticas

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
+import csv
 import gym
+import matplotlib.pyplot as plt
+import pandas as pd
 
 from dqn import Dqn
 from ddqn import Ddqn
@@ -16,10 +19,23 @@ def run_episode(env, agent, load_path=None):
         state, _, done, _ = env.step(action)
     env.close()
 
+def plot_stats(stats, smoothing_window=10):
+    fig = plt.figure(figsize=(10,5))
+    # stats_smoothed = pd.Series(stats).rolling(smoothing_window, min_periods=smoothing_window).mean()
+    # plt.plot(stats_smoothed)
+    plt.plot(stats)
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.title("")
+    plt.show()
+
 if __name__ == '__main__':
-    env = gym.make('CartPole-v1')
+    # env = gym.make('CartPole-v1')
     # agent = DdqnExperienceReplayPrioritized(env)
-    agent = Dqn(env)
-    agent.train(episodes_num=1000)
+    # agent = Dqn(env)
+    # agent.train(episodes_num=1000)
     # load_path = 'app/saves/experience_replay_prioritized/episode700.h5'
     # run_episode(env, agent, load_path=load_path)
+
+    df = pd.read_csv('app/saves/dqn/scores.csv', header=None, names=['sc'])
+    plot_stats(df['sc'])
